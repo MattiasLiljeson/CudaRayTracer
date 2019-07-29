6b968b67189c3f949f6508d89b44182aa5b6e50d
Mattias Liljeson <mattiasliljeson@gmail.com>
2019-07-29 16:40:46 +0200

    Move classes to their own files

---
+++ cudaTracer/CudaMesh.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Vertex.cuh"
+#include "GlobalCudaVector.h"
+#include "Texture.cuh"
+#include "Mesh.cuh"
+#include "Shape.cuh"
+#include "Model.h"
+#include <lodepng.h>
+#include <vector>
+
+class CudaMesh {
+   public:
+    GlobalCudaVector<Vertex> vertices;
+    GlobalCudaVector<int> indices;
+    GlobalCudaVector<unsigned char> textureData;
+    Texture texture;
+    Mesh mesh;
+    Shape shape;
+
+    CudaMesh(Model model) {
+        vertices = GlobalCudaVector<Vertex>::fromVector(model.getVertices());
+        indices = GlobalCudaVector<int>::fromVector(model.getIndices());
+        // TODO: 3 is just a happy guess
+        int triCnt = model.getNumVertices() / 3;
+
+        std::string texFname = model.getMaterials()[0].texturePath;
+        std::vector<unsigned char> image;  // the raw pixels
+        unsigned width, height;
+        unsigned error = lodepng::decode(image, width, height, texFname);
+        // if there's an error, display it. TODO: Replace this with something
+        // proper..
+        if (error) {
+            std::cout << "decoder error " << error << ": "
+                      << lodepng_error_text(error) << std::endl;
+        }
+        textureData = GlobalCudaVector<unsigned char>::fromVector(image);
+        texture = Texture(width, height, textureData.getDevMem());
+        mesh = Mesh(indices.getDevMem(), triCnt, vertices.getDevMem(), texture);
+        shape = Shape(mesh);
+        shape.material.materialType = Object::DIFFUSE_AND_GLOSSY;
+    }
+
+    CudaMesh(std::vector<Vertex> vertices, std::vector<int> indices,
+             int p_triCnt, std::vector<unsigned char> textureData, int texWidth,
+             int texHeight) {
+        this->vertices = GlobalCudaVector<Vertex>::fromVector(vertices);
+        this->indices = GlobalCudaVector<int>::fromVector(indices);
+        this->textureData =
+            GlobalCudaVector<unsigned char>::fromVector(textureData);
+        texture = Texture(texWidth, texHeight, this->textureData.getDevMem());
+        this->mesh = Mesh(this->indices.getDevMem(), p_triCnt,
+                          this->vertices.getDevMem(), texture);
+        shape = Shape(mesh);
+        shape.material.materialType = Object::DIFFUSE_AND_GLOSSY;
+    }
+};
--- cudaTracer/Mesh.cuh
+++ cudaTracer/Mesh.cuh
@@ -4,38 +4,9 @@
 #include <cuda_runtime.h>
 #include "Object.cuh"
 #include "Vertex.cuh"
+#include "Texture.cuh"
 
 using namespace axis;
-
-struct Texture {
-    int width;
-    int height;
-    unsigned char *data;
-
-    __host__ __device__ Texture() {
-        width = -1;
-        height = -1;
-        data = NULL;
-    }
-
-    __host__ __device__ Texture(unsigned width, unsigned height,
-                                unsigned char *data) {
-        this->data = data;
-        this->width = width;
-        this->height = height;
-    }
-
-    __device__ Vec3f sample(float u, float v) const {
-        // nearest neighbor for now. TODO: support bilinear at least
-        int x = (int)round(u * width) % width;
-        int y = (int)round(v * height) % height;
-        float r = data[y * width * 4 + x * 4] / 255.0f;
-        float g = data[y * width * 4 + x * 4 + 1] / 255.0f;
-        float b = data[y * width * 4 + x * 4 + 2] / 255.0f;
-        return Vec3f(r, g, b);
-        // return Vec3f(x/512.0f, , 1.0f);
-    }
-};
 
 class Mesh {
    public:
