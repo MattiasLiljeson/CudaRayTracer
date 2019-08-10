cf2457dbd242130e3c8c7bd9a913d0aee4ec4f0f
Mattias Liljeson <mattiasliljeson@gmail.com>
2019-08-10 12:00:38 +0200

    Fix PicDump bug and change experiment setup
    PicDump didn't work as the array it was allocating was of the wrong size
    
    The experiments are now loaded from json files and are not defined in
    code. The reason for this is to make sure that each expirement is
    executed from a clean slate. There should be no risk of uncleaned, old
    memory, laying around on the device or host. This cahnge is to mitigate
    this risk.

--- cudaTracer/main.cpp
+++ cudaTracer/main.cpp
@@ -1,28 +1,24 @@
 #include "preProc.h"
 
-#include "DeviceHandler.h"
-#include "cudaUtils.h"
-
+#include <D3D11SDKLayers.h>
 #include <cuda_d3d11_interop.h>
 #include <cuda_runtime_api.h>
-#include "TextureRenderer.h"
-#include "globals.h"
-
-#include <D3D11SDKLayers.h>
-#include "D3DDebugger.h"
-
-#include "DebugGUI.h"
-#include "Profiler.h"
-#include "thrust\system\system_error.h"
-
 #include <lodepng.h>
 #include "Camera.h"
-
+#include "D3DDebugger.h"
+#include "DebugGUI.h"
+#include "DeviceHandler.h"
+#include "ExperimentReader.h"
 #include "PicDumper.h"
-
 #include "Popup.h"
+#include "Profiler.h"
 #include "RayTracer.h"
 #include "Statistics.h"
+#include "TextureRenderer.h"
+#include "cudaUtils.h"
+#include "globals.h"
+#include "guicon.h"
+#include "thrust\system\system_error.h"
 
 void prepareServices(HINSTANCE hInstance, DeviceHandler* deviceHandler,
                      Options options) {
@@ -35,7 +31,7 @@ void prepareServices(HINSTANCE hInstance, DeviceHandler* deviceHandler,
     static RayTracer tracer(texRender.getTextureSet(), options.device.width,
                             options.device.height, options);
     static Statistics stats;
-    static PicDumper dumper(options.device.height, options.device.height);
+    static PicDumper dumper(options.device.width, options.device.height);
 
     ServiceRegistry::instance().add<InputHandler>(&input);
     ServiceRegistry::instance().add<RayTracer>(&tracer);
@@ -45,104 +41,32 @@ void prepareServices(HINSTANCE hInstance, DeviceHandler* deviceHandler,
     ServiceRegistry::instance().add<DebugGUI>(&dg);
 }
 
-struct ExperimentParameters {
-    std::string experimentName;
-    std::string phaseName;
-    Options options;
-};
-
-std::vector<std::function<ExperimentParameters(Options, int)>>
-createExperiments() {
-    std::vector<std::function<ExperimentParameters(Options, int)>> experiments;
-    {
-        auto blockSize = [](Options o, int i) -> ExperimentParameters {
-            int blocksize[5] = {4, 8, 12, 16, 20};
-            o.device.blockSize = blocksize[i];
-            const int BUFF_SIZE = 128;
-            char buf[BUFF_SIZE];
-            sprintf_s(buf, BUFF_SIZE, "block_size_%d", blocksize[i]);
-            return {"block_size", std::string(buf), o};
-        };
-        experiments.push_back(blockSize);
-    }
-    {
-        auto resolutionTest = [](Options o, int i) -> ExperimentParameters {
-            Vec2i resolutions[5] = {
-                {200, 150}, {400, 300}, {600, 450}, {800, 600}, {1000, 750}};
-            o.device.width = resolutions[i][X];
-            o.device.height = resolutions[i][Y];
-            const int BUFF_SIZE = 128;
-            char buf[BUFF_SIZE];
-            sprintf_s(buf, BUFF_SIZE, "resolution_%d-%d", resolutions[i][X],
-                      resolutions[i][Y]);
-            return {"resolution", std::string(buf), o};
-        };
-        experiments.push_back(resolutionTest);
-    }
-    {
-        auto maxTraceDepth = [](Options o, int i) -> ExperimentParameters {
-            int depths[5] = {1, 2, 3, 4, 5};
-            o.device.maxDepth = depths[i];
-            const int BUFF_SIZE = 128;
-            char buf[BUFF_SIZE];
-            sprintf_s(buf, BUFF_SIZE, "max_trace_depth_%d", depths[i]);
-            return {"max_trace_depth", std::string(buf), o};
-        };
-        experiments.push_back(maxTraceDepth);
-    }
-    {
-        auto lights = [](Options o, int i) -> ExperimentParameters {
-            int lights[5] = {10, 20, 30, 40, 50};
-            o.host.lightCnt = lights[i];
-            const int BUFF_SIZE = 128;
-            char buf[BUFF_SIZE];
-            sprintf_s(buf, BUFF_SIZE, "lights_%d", lights[i]);
-            return {"lights", std::string(buf), o};
-        };
-        experiments.push_back(lights);
-    }
-    {
-        auto triangles = [](Options o, int i) -> ExperimentParameters {
-            pair<std::string, std::string> models[5] = {
-                {"../assets/models/metalBarrel/", "metal_barrel.obj"},
-                {"../assets/models/metalBarrel/", "metal_barrel.obj"},
-                {"../assets/models/metalBarrel/", "metal_barrel.obj"},
-                {"../assets/models/metalBarrel/", "metal_barrel.obj"},
-                {"../assets/models/metalBarrel/", "metal_barrel.obj"},
-            };
-            o.host.modelFolder = models[i].first;
-            o.host.modelFname = models[i].second;
-            const int BUFF_SIZE = 128;
-            char buf[BUFF_SIZE];
-            sprintf_s(buf, BUFF_SIZE, "model_%s", models[i].second);
-            return {"model", std::string(buf), o};
-        };
-        experiments.push_back(triangles);
-    }
-    return experiments;
-}
-
-#ifndef _TEST
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nCmdShow) {
+Options loadDefaults() {
     Options options;
     options.host.lightCnt = 10;
     options.host.fov = 90;
-    options.device.width = 800;
-    options.device.height = 600;
+    options.device.width = 1024;
+    options.device.height = 768;
     options.device.maxDepth = 5;
-    options.device.samples = 1;
+    options.device.samples = 12;
     options.device.backgroundColor = Vec3f(0.2f, 0.7f, 0.8f);
     options.device.shadowBias = 0.0001f;
+    options.device.blockSize = 4;
+
     options.device.scale = tan(deg2rad(options.host.fov * 0.5f));
     options.device.imageAspectRatio =
         options.device.width / (float)options.device.height;
     options.device.gammaCorrection = true;
-    options.device.blockSize = 16;
+    return options;
+}
 
-    auto experiments = createExperiments();
+#ifndef _TEST
+INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+            int nCmdShow) {
+    RedirectIOToConsole();
+    Options options = loadDefaults();
 
-    bool doExperiment = true;
+    bool doExperiment = !string(lpCmdLine).empty();
     if (!doExperiment) {
         // HACK: add space for borders
         int wndWidth = options.device.width + 16;
@@ -168,31 +92,43 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
             }
         }
     } else {
-        Profiler* profiler = Profiler::instance();
-        const int experimentRounds = 100;
-        const int experimentStages = 5;
-        for (auto& exp : experiments) {
-            for (int i = 0; i < experimentStages; ++i) {
-                ExperimentParameters e = exp(options, i);
-                profiler->addPerfTimer(e.phaseName, e.experimentName, true);
-                // HACK: add space for borders
-                int wndWidth = options.device.width + 16;
-                // HACK: add space for borders and header
-                int wndHeight = options.device.height + 39;
-
-                DeviceHandler deviceHandler(hInstance, wndWidth, wndHeight);
-                D3DDebugger d3dDbg(deviceHandler.getDevice());
-                prepareServices(hInstance, &deviceHandler, e.options);
-                for (int j = 0; j < experimentRounds; ++j) {
-                    profiler->start(e.phaseName, e.experimentName, true);
-                    deviceHandler.beginDrawing();
-                    ServiceRegistry::instance().updateAll(0.016f);
-                    deviceHandler.presentFrame();
-                    profiler->stop(e.phaseName, e.experimentName, true);
-                }
-            }
+        std::stringstream stream;
+        stream << lpCmdLine;
+        std::string experimentFname;
+        int phaseIdx = 0;
+        if (stream.rdbuf()->in_avail() != 0) {
+            stream >> experimentFname;
+            stream >> phaseIdx;
         }
-        profiler->logTimersToFile(true, true, "results");
+        ExperimentReader reader(experimentFname, options);
+        ExperimentParameters exp = reader.experiment;
+        cout << reader;
+        Profiler* profiler = Profiler::instance();
+        const int experimentRounds = 1;
+
+        std::cout << "running experiment: " << exp.experimentName << std::endl;
+        ExperimentPhase phase = exp.phases[phaseIdx];
+        std::cout << "\tPhase: " << phase.phaseName << std::endl;
+        profiler->addPerfTimer(phase.phaseName, exp.experimentName, true);
+        // HACK: add space for borders
+        int wndWidth = options.device.width + 16;
+        // HACK: add space for borders and header
+        int wndHeight = options.device.height + 39;
+
+        DeviceHandler deviceHandler(hInstance, wndWidth, wndHeight);
+        D3DDebugger d3dDbg(deviceHandler.getDevice());
+        prepareServices(hInstance, &deviceHandler, phase.opts);
+        for (int j = 0; j < experimentRounds; ++j) {
+            profiler->start(phase.phaseName, exp.experimentName, true);
+            deviceHandler.beginDrawing();
+            ServiceRegistry::instance().updateAll(0.016f);
+            ServiceRegistry::instance().get<PicDumper>()->toDisk(
+                ServiceRegistry::instance().get<TextureRenderer>());
+            deviceHandler.presentFrame();
+            profiler->stop(phase.phaseName, exp.experimentName, true);
+        }
+
+        profiler->logTimersToFile(false, exp.experimentName);
         // d3dDbg.reportLiveDeviceObjects();
     }
     return 0;
